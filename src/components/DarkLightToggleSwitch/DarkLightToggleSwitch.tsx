f70508d0d8e9f7bf94bcafa232c7c07abb0cfd20
atorcode <atorcode@gmail.com>
2023-03-20 06:32:12 -0700

    Implement DarkLightToggleSwitch

--- src/components/DarkLightToggleSwitch/DarkLightToggleSwitch.tsx
+++ src/components/DarkLightToggleSwitch/DarkLightToggleSwitch.tsx
@@ -5,9 +5,23 @@ import styles from "./DarkLightToggleSwitch.module.scss";
 import { RiSunFill } from "react-icons/ri";
 import { RiMoonFill } from "react-icons/ri";
 
+// hooks
+import { useRef, useState } from "react";
+
 const DarkLightToggleSwitch = () => {
+  const [isDark, setIsDark] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
   return (
-    <button className={styles["panel"]}>
+    <button
+      className={styles["panel"]}
+      ref={buttonRef}
+      onClick={() => {
+        isDark
+          ? buttonRef.current?.classList.remove(styles["dark-mode"])
+          : buttonRef.current?.classList.add(styles["dark-mode"]);
+        setIsDark((prev) => !prev);
+      }}
+    >
       <RiMoonFill />
       <RiSunFill />
       <div className={styles["switch"]}></div>
