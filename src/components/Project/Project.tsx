76db8a478becc44bd966ec31488cfaa9675a94d7
atorcode <atorcode@gmail.com>
2023-04-11 14:20:04 -0700

    Add images for each project and dynamically import them in the Project component

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,17 +1,29 @@
 // styles
 import styles from "./Project.module.scss";
 
+// hooks
+import { useEffect, useState } from "react";
+
 // components
 import Button from "../Button";
 
 // types
 import { ProjectType as ProjectProps } from "../../types/ProjectType";
 
-const Project = ({ name, url }: ProjectProps): JSX.Element => {
+const Project = ({ name, imagePath, url }: ProjectProps): JSX.Element => {
+  const [projectImage, setProjectImage] = useState<string | undefined>();
+  useEffect(() => {
+    const loadImage = async (): Promise<void> => {
+      const importedImage = await import(`../../assets/${imagePath}`);
+      setProjectImage(importedImage.default);
+    };
+    loadImage();
+  }, [imagePath]);
+
   return (
     <article className={styles["project"]}>
       <h2>{name}</h2>
-      <img src="" />
+      <img src={projectImage} className={styles["project-image"]} />
       <Button text="View Project" url={url} />
     </article>
   );
--- src/types/ProjectType.ts
+++ src/types/ProjectType.ts
@@ -1,5 +1,6 @@
 export type ProjectType = {
   id: number;
   name: string;
+  imagePath: string;
   url: string;
 };
