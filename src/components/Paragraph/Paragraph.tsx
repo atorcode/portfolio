1be2727fd3a899b7a3d20e932c3e439f172cf3d8
atorcode <atorcode@gmail.com>
2023-04-24 07:42:31 -0700

    Create color-transition mixin and change color of Paragraph component based on theme

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,12 +1,20 @@
 // styles
 import styles from "./Paragraph.module.scss";
 
+// hooks
+import { useThemeContext } from "../../contexts/ThemeContext";
+
 type ParagraphProps = {
   text: string;
 };
 
 const Paragraph = ({ text }: ParagraphProps): JSX.Element => {
-  return <p className={styles["paragraph"]}>{text}</p>;
+  const { theme } = useThemeContext();
+  return (
+    <p className={`${styles["paragraph"]} ${styles[`paragraph-${theme}`]}`}>
+      {text}
+    </p>
+  );
 };
 
 export default Paragraph;
