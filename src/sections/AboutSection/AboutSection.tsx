d70d86527f0f92ed9b6db3eea26213313a5754e4
atorcode <atorcode@gmail.com>
2023-05-04 17:08:35 -0700

    Greatly improve mobile layout and make sizes and spacing more consistent

--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -21,8 +21,10 @@ const AboutSection = (): JSX.Element => {
       ref={aboutSectionRef}
     >
       <SectionHeading text="About Me" />
-      <Paragraph text="Front-end web development is the perfect intersection between the analytic and the creative, which suits me well. I care deeply about improving user experience, and I have a methodical approach when coding which forces me to pay meticulous attention to detail. You can find me constantly daydreaming about ways to make code cleaner and more reusable." />
-      <Paragraph text="I strive to understand how everything works, and the most fun way to learn is together. I regularly reinforce my mastery of concepts and techniques by teaching, and I am grateful to my peers and the programming community at large for being so welcoming and for accelerating my learning. These attitudes along with my desire to do things the right way underlie the great products and services that I've developed." />
+      <div>
+        <Paragraph text="Front-end web development is the perfect intersection between the analytic and the creative, which suits me well. I care deeply about improving user experience, and I have a methodical approach when coding which forces me to pay meticulous attention to detail. You can find me constantly daydreaming about ways to make code cleaner and more reusable." />
+        <Paragraph text="I strive to understand how everything works, and the most fun way to learn is together. I regularly reinforce my mastery of concepts and techniques by teaching, and I am grateful to my peers and the programming community at large for being so welcoming and for accelerating my learning. These attitudes along with my desire to do things the right way underlie the great products and services that I've developed." />
+      </div>
       <Button
         text="View Résumé"
         url="https://drive.google.com/file/d/1wkYO592dsnyJ2kPbKfhvp_Di63QPhHJ_/view?usp=share_link"
