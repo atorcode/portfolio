77fbd0fcf30592f642ab37ad042b685daf76e32f
atorcode <atorcode@gmail.com>
2023-05-07 21:23:03 -0700

    Add smooth transitions to colors when changing theme

--- src/components/NavBullet/NavBullet.tsx
+++ src/components/NavBullet/NavBullet.tsx
@@ -2,6 +2,7 @@
 import styles from "./NavBullet.module.scss";
 
 // hooks
+import { useEffect, useRef } from "react";
 import { useScrollContext } from "../../contexts/ScrollContext";
 import { useThemeContext } from "../../contexts/ThemeContext";
 
@@ -10,8 +11,26 @@ type NavBulletProps = {
 };
 
 const NavBullet = ({ index }: NavBulletProps) => {
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
   const { expandedBulletIndex, scrollToIndex } = useScrollContext();
   const { theme } = useThemeContext();
+
+  useEffect((): (() => void) => {
+    if (index === expandedBulletIndex) {
+      buttonRef.current?.classList.add(
+        `${styles["bullet-expanded-transition"]}`
+      );
+    } else {
+      buttonRef.current?.classList.add(`${styles["bullet-transition"]}`);
+    }
+
+    return (): void => {
+      buttonRef.current?.classList.remove(
+        `${styles["bullet-expanded-transition"]}`
+      );
+      buttonRef.current?.classList.remove(`${styles["bullet-transition"]}`);
+    };
+  }, [theme]);
 
   return (
     <button
@@ -21,9 +40,10 @@ const NavBullet = ({ index }: NavBulletProps) => {
             ? `${styles["bullet"]} ${styles["bullet-expanded"]} ${
                 styles[`bullet-expanded-${theme}`]
               }`
-            : styles["bullet"]
+            : `${styles["bullet"]}`
         }`}
       tabIndex={-1}
+      ref={buttonRef}
       onClick={(): void => scrollToIndex(index)}
     ></button>
   );
