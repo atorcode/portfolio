744dce61a9a73596b1ddabe8951a2dac715c9bc2
atorcode <atorcode@gmail.com>
2023-05-24 03:05:42 -0700

    Add Github profile link to HeaderMenu and add transition

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,6 +1,13 @@
 // styles
 import styles from "./HeaderMenu.module.scss";
 
+// icons
+import { FaGithub } from "react-icons/fa";
+
+// hooks
+import { useEffect, useRef } from "react";
+import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
+
 // components
 import DarkLightToggleSwitch from "../DarkLightToggleSwitch";
 
@@ -12,8 +19,32 @@ const HeaderMenu = ({
   isVisible,
   setIsVisible,
 }: HeaderMenuProps): JSX.Element => {
+  const githubRef = useRef<HTMLAnchorElement | null>(null);
+
+  useEffect(() => {
+    githubRef.current?.classList.add(styles["github-link-before"]);
+  }, []);
+
+  useIntersectionObserver({
+    ref: githubRef,
+    isVisible,
+    setIsVisible,
+    transitionDelay: 500,
+    beforeTransitionClass: styles["github-link-before"],
+    afterTransitionClass: styles["github-link-after"],
+  });
+
   return (
     <header className={styles["header"]}>
+      <a
+        className={styles["github-link"]}
+        href={"https://github.com/atorcode"}
+        ref={githubRef}
+        target="_blank"
+      >
+        <FaGithub />
+      </a>
+
       <DarkLightToggleSwitch
         isVisible={isVisible}
         setIsVisible={setIsVisible}
